4ba8411cc36c9372c5fe49c19c93b059a560e195
Loïc Girault <lgirault@mediarithmics.com>
2018-03-01 20:55:25 +0100

    weeeeeeep

--- src/main/scala/mh/Main.scala
+++ src/main/scala/mh/Main.scala
@@ -19,6 +19,38 @@ case class Input(numRows: Int, numCols: Int, numVehicles: Int, numRides: Int, pe
       |
       |${rides.mkString("\n")}
       |""".stripMargin
+
+  lazy  val ridesSet: Set[Ride] = rides.toSet
+
+  def graph(nth: Int = 0, visited: Set[Ride] = Set.empty,
+            currentX: Int = 0,
+            currentY: Int = 0,
+            currentTime: Int = 0) : Set[Node] =
+  if(nth >= numRides) Set.empty
+  else {
+
+    for {
+      r <- ridesSet -- visited
+    } yield {
+
+
+      val newVisited = visited + r
+      val nexts = (ridesSet -- newVisited).map {
+        n =>
+          val goTo = Utils.distTime(currentX, currentY, r.xStart, r.yStart)
+
+          val nextActualStart = Math.max(currentTime + goTo, n.earliestStart)
+          val nextActualEndTime = nextActualStart + n.distTime
+          Node(n, nth,
+            graph(nth + 1, newVisited, n.xFinish, n.yFinish,
+              currentTime + goTo + n.distTime),
+            nextActualStart,
+            nextActualEndTime)
+      }
+      Node(r, nth, nexts)
+    }
+
+  }
 }
 
 object Utils {
@@ -29,6 +61,7 @@ object Utils {
   def distTimeBetween(r1: Ride, r2: Ride) = distTime(r1.xFinish, r1.yFinish, r2.xStart, r2.yStart)
 }
 
+case class Node(r: Ride, nth: Int, neighbours: Set[Node], actualStartTime: Int, actualEndTime: Int)
 
 object Main {
 
@@ -50,8 +83,12 @@ object Main {
     Input(numRows.toInt, numCols.toInt, numVehicles.toInt, numRides.toInt, perRideBonusOnStart.toInt, numSteps.toInt, rides)
   }
 
+
+
   def main(args: Array[String]) : Unit = {
     val exUrl = getClass.getClassLoader.getResource("a_example.in")
-    println(read(exUrl.toURI))
+    val input = read(exUrl.toURI)
+
+    println(input.graph().mkString("\n"))
   }
 }
