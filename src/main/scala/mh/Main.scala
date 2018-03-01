aece0cb08e09ce3577bf1d9fd808f1ddeb426c2e
Loïc Girault <lgirault@mediarithmics.com>
2018-03-01 19:09:54 +0100

    parse

--- src/main/scala/mh/Main.scala
+++ src/main/scala/mh/Main.scala
@@ -1,8 +1,29 @@
 package mh
 
+import scala.io.Source
+
+case class Ride(xStart: Int, yStart: Int, xFinsh: Int, yFinish: Int, earliestStart: Int, latestFinish: Int)
+case class Input(numRows: Int, numCols: Int, numVehicles: Int, numRides: Int, perRideBonusOnStart: Int, numSteps: Int,
+                 rides: Vector[Ride])
+
 object Main {
+  def parse(source: Source) = {
+    val bufIterator = source.getLines()
+
+    val header = bufIterator.next().split(" ")
+
+    val Array(numRows, numCols, numVehicles, numRides, perRideBonusOnStart, numSteps) = header
+
+    val rides = bufIterator.foldLeft(Vector.empty[Ride]) {
+      case (rs, line) =>
+        val Array(xStart, yStart, xFinish, yFinish, earliestStart, latestFinish) = line.split(" ")
+        rs :+ Ride(xStart.toInt, yStart.toInt, xFinish.toInt, yFinish.toInt, earliestStart.toInt, latestFinish.toInt)
+    }
+
+    Input(numRows.toInt, numCols.toInt, numVehicles.toInt, numRides.toInt, perRideBonusOnStart.toInt, numSteps.toInt, rides)
+  }
 
   def main(args: Array[String]) : Unit = {
-    println("Hello, World!")
+
   }
 }
