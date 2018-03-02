e290a2af3b0e18ff1ecb10e2e99c0fe5fcf11497
skireev <skireev@mediarithmics.com>
2018-03-02 10:23:59 +0100

    last commit

--- src/main/scala/mh/Main.scala
+++ src/main/scala/mh/Main.scala
@@ -4,7 +4,7 @@ import java.net.URI
 
 import scala.io.Source
 
-case class Ride(xStart: Int, yStart: Int, xFinish: Int, yFinish: Int, earliestStart: Int, latestFinish: Int){
+case class Ride(id: Int, xStart: Int, yStart: Int, xFinish: Int, yFinish: Int, earliestStart: Int, latestFinish: Int){
   def distTime = Utils.distTime(xStart, yStart, xFinish, yFinish)
 
   override def toString: String = s"[tc = ${distTime}, start = ${earliestStart}, finish = ${latestFinish} ]"
@@ -17,7 +17,6 @@ case class Input(numRows: Int, numCols: Int, numVehicles: Int, numRides: Int, pe
       |${perRideBonusOnStart} perRideBonusOnStart
       |${numSteps} steps
       |
-      |${rides.mkString("\n")}
       |""".stripMargin
 
   lazy  val ridesSet: Set[Ride] = rides.toSet
@@ -26,7 +25,7 @@ def getNeighbors(nth: Int = 0,
                  visited: Set[Ride] = Set.empty,
                  currentX: Int = 0,
                  currentY: Int = 0,
-                 currentTime: Int = 0) : Set[Node] =
+                 currentTime: Int = 0)(implicit input: Input) : Set[Node] =
   if(nth >= numRides) Set.empty
   else {
     for {
@@ -36,12 +35,22 @@ def getNeighbors(nth: Int = 0,
       val goTo = Utils.distTime(currentX, currentY, neighbor.xStart, neighbor.yStart)
       val nextActualStart = Math.max(currentTime + goTo, neighbor.earliestStart)
       val nextActualEndTime = nextActualStart + neighbor.distTime
-      Node(neighbor,
-           nth+1,
-           getNeighbors(nth+1, newVisited, neighbor.xFinish, neighbor.yFinish, nextActualEndTime),
-            nextActualStart,
-            nextActualEndTime,
-           )
+
+      println(neighbor)
+      val neighbors: Set[Node] =
+        if (neighbor.latestFinish <= nextActualEndTime && neighbor.earliestStart >= nextActualStart)
+          getNeighbors(nth+1, newVisited, neighbor.xFinish, neighbor.yFinish, nextActualEndTime)
+        else
+          Set.empty
+
+      val tempNode = Node(neighbor,
+                          nth+1,
+                          neighbors,
+                          nextActualStart,
+                          nextActualEndTime, 0)
+
+      val maxNeighborScore = if (neighbors.isEmpty) 0 else neighbors.maxBy(_.score).score
+      tempNode.copy(score = Node.evaluateNeighbor(tempNode) + maxNeighborScore)
     }
   }
 }
@@ -54,6 +63,22 @@ object Utils {
   def distTimeBetween(r1: Ride, r2: Ride) = distTime(r1.xFinish, r1.yFinish, r2.xStart, r2.yStart)
 }
 
+object Node {
+  def evaluateNeighbor(neighbor: Node)(implicit input: Input): Int = {
+    val ride = neighbor.ride
+    val bonus =
+      if (neighbor.actualStartTime <= ride.earliestStart)
+        input.perRideBonusOnStart
+      else
+        0
+
+    val rideValue = ride.distTime
+    if (neighbor.actualEndTime <= ride.latestFinish)
+      (bonus + rideValue)
+    else 0
+  }
+}
+
 case class Node(ride: Ride, nth: Int, neighbours: Set[Node], actualStartTime: Int, actualEndTime: Int, score: Int) {
   override def toString(): String = {
     s"""
@@ -61,30 +86,16 @@ case class Node(ride: Ride, nth: Int, neighbours: Set[Node], actualStartTime: In
        |   $neighbours
      """.stripMargin
   }
-
-  def evaluateNeighbor(neighbor: Node)(implicit input: Input): Int = {
-    val ride = neighbor.ride
-    val bonus =
-      if (actualStartTime <= ride.earliestStart)
-        input.perRideBonusOnStart
-      else
-        0
-
-    val rideValue = ride.distTime
-    if (actualEndTime <= ride.latestFinish)
-      (bonus + rideValue)
-    else 0
-  }
 }
 
 object Dijkstra {
-  val Priority
+  def maxPath(graph : Set[Node], result: List[Node] = Nil, finished: Set[Int] = Set.empty): List[Node] = {
+    val filtered = graph.filterNot(n => finished.contains(n.ride.id))
 
-  def maxPath(graph : Set[Node]): Set[Node] = {
-    graph.map {
-      node =>
-        node.neighbours.map()
-
+    if(filtered.isEmpty) result.reverse
+    else {
+      val bestNeighbor = filtered.maxBy(_.score)
+      maxPath(bestNeighbor.neighbours, bestNeighbor :: result)
     }
   }
 }
@@ -100,11 +111,11 @@ object Main {
 
     val Array(numRows, numCols, numVehicles, numRides, perRideBonusOnStart, numSteps) = header
 
-    val rides = bufIterator.foldLeft(Vector.empty[Ride]) {
-      case (rs, line) =>
+    val rides = bufIterator.foldLeft((Vector.empty[Ride], 0)) {
+      case ((rs, i), line) =>
         val Array(xStart, yStart, xFinish, yFinish, earliestStart, latestFinish) = line.split(" ")
-        rs :+ Ride(xStart.toInt, yStart.toInt, xFinish.toInt, yFinish.toInt, earliestStart.toInt, latestFinish.toInt)
-    }
+        (rs :+ Ride(i, xStart.toInt, yStart.toInt, xFinish.toInt, yFinish.toInt, earliestStart.toInt, latestFinish.toInt), i + 1)
+    }._1
 
     Input(numRows.toInt, numCols.toInt, numVehicles.toInt, numRides.toInt, perRideBonusOnStart.toInt, numSteps.toInt, rides)
   }
@@ -112,9 +123,17 @@ object Main {
 
 
   def main(args: Array[String]) : Unit = {
-    val exUrl = getClass.getClassLoader.getResource("a_example.in")
-    val input = read(exUrl.toURI)
 
-    println(input.getNeighbors().mkString("\n"))
+    val exUrl = getClass.getClassLoader.getResource("a_example.in")
+    implicit val input = read(exUrl.toURI)
+    println(input)
+    val graph = input.getNeighbors()
+   // println(input.getNeighbors().mkString("\n"))
+    val output = Range(0, input.numVehicles).foldLeft((Set.empty[Int], List.empty[List[Int]])){
+      case ((visited, acc), _) =>
+        val path = Dijkstra.maxPath(graph, Nil, visited).map(_.ride.id)
+        (visited ++ path, path::acc)
+    }._2
+    println(s"${output.map(rides => rides.length + " " + rides.mkString(" ")).mkString("\n")}")
   }
 }
